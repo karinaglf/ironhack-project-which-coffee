b23a0b2a4f697fe5a6c904d432a24664ac54a13c
Clara Vasconcelos <claraeclara@gmail.com>
2021-11-19 14:57:46 +0000

    Update auth route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,8 @@
 const router = require("express").Router();
 
-<<<<<<< HEAD
-// ROUTES router
-
-//GET  /signup / router
-=======
 // ROUTE
 
 //GET  /signup -
->>>>>>> a26e4f516539e3a63cf72875a7cf1c3a1e73715e
 router.get("/signup", (req, res) => {
   res.render("auth/signup-form");
 });
