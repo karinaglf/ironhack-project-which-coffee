7e80e495ccfff2c4a394de7b4a15fb6c69206daa
Karina Freitas <karinaglf@gmail.com>
2021-11-20 11:35:41 +0000

    add signup-view

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,20 @@
-const router = require("express").Router();
+// routes/auth.routes.js
+const router = require('express').Router();
+const User = require('./../models/User.model');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 // ROUTE
 
+// GET  /signup
+router.get('/signup', (req, res) => {
+  res.render('auth/signup-form');
+});
+
 //GET  /signup -
-router.get("/signup", (req, res) => {
-  res.render("auth/signup-form");
+router.get('/signup', (req, res) => {
+  res.render('auth/signup-form');
 });
 
 module.exports = router;
