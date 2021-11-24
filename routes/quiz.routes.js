36e8978e1c4081cc4da7b03b376b453f900c2c98
Clara Vasconcelos <claraeclara@gmail.com>
2021-11-24 18:48:07 +0000

    create quiz logic

--- routes/quiz.routes.js
+++ routes/quiz.routes.js
@@ -1,22 +1,32 @@
 const router = require('express').Router();
 const Coffee = require('../models/Coffee.model');
+const User = require('./../models/User.model');
 
 router.get('/quiz', (req, res) => {
   res.render('quiz/quiz-form');
 });
 
-/*router.post('/quiz', (req, res) => {
-  switch (quizResult) {
-    case x:
-      // code block
-      break;
-    case y:
-      // code block
-      break;
-    default:
-    // code block
-  }
+router.post('/quiz', (req, res) => {
+  const { q_1, q_2, q_3, q_4, q_5, q_6 } = req.body;
+  const combination = q_1 + q_2 + q_3 + q_4 + q_5; // "abababa"
+
+  // console.log('combination', combination);
+
+  const dictionary = {
+    aaaaa: ['Ethiopia', 'Kenya', 'Rwanda'],
+    abaaa: ['Ethiopia'],
+    aabaa: ['Kenya', 'rwanda'],
+  };
+  const originCountries = dictionary[combination]; // dictionary["aaaaaa"]   or dictioinary.aaaaaa
+
+  Coffee.find({ originCountry: { $in: originCountries } }) //
+  .then(
+    (foundCoffees) => {
+      res.render('coffees/coffees-listing', {
+        coffeesList: foundCoffees,
+      });
+    }
+  );
 });
-*/
 
 module.exports = router;
