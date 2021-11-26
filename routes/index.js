dca458aa96f21c7a14993e2391df5a8231fd454b
Karina Freitas <karinaglf@gmail.com>
2021-11-26 01:07:36 +0000

    add random coffees on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
 const isLoggedIn = require('./../middleware/isLoggedIn');
 const User = require('./../models/User.model');
+const Coffee = require('../models/Coffee.model');
+
 
 //GET - home page
 router.get('/', (req, res, next) => {
   const user = req.session.user;
-  res.render('index', { user });
+
+  Coffee.find()
+    .then((coffeeList) => {
+
+      let n = 3;
+      let randomCoffees = coffeeList.sort(() => Math.random() - Math.random()).slice(0, n)
+
+      console.log(`coffeeList`, randomCoffees )
+      res.render('index', { user , randomCoffees});
+    })
+
 });
 
 //GET - profile page
